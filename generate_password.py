edb9fe3fd38bc4c60852bd651912346bd46cea01
tipromon <154251431+tipromon@users.noreply.github.com>
2024-09-17 19:59:57 -0300

    Novo usuário
    Usuário pra Fabiana

--- generate_password.py
+++ generate_password.py
@@ -3,11 +3,11 @@ import pickle
 from pathlib import Path
 
 # Nomes e usuários para autenticação
-names = ["Peter Parker", "Rebecca Miller", "Michel Daros", "Gustavo Pelissaro", "Alex Sandoval", "Alexsandra Mendes", "Marco Lamim"]
-usernames = ["pparker", "rmiller", "mdaros", "gpelissaro", "asandoval", "amendes", "mlamim"]
+names = ["Peter Parker", "Rebecca Miller", "Michel Daros", "Gustavo Pelissaro", "Alex Sandoval", "Alexsandra Mendes", "Marco Lamim", "Guilherme Grandesi", "Henrique Riego", "Rogerio Ishikawa", "David Andrade", "Fabiana Garcia"]
+usernames = ["pparker", "rmiller", "mdaros", "gpelissaro", "asandoval", "amendes", "mlamim", "ggrandesi", "hriego", "rishikawa", "dandrade", "fgarcia"]
 
 # Definir as senhas (não hashadas) que serão usadas
-passwords = ["abc123", "def456", "michelpassword", "gustavopassword", "alexpassword", "alexsandrapassword", "marcopassword"]
+passwords = ["abc123", "def456", "michelpassword", "gustavopassword", "alexpassword", "alexsandrapassword", "marcopassword", "guilherme123", "henrique123", "rogerio123", "david123", "fabiana123"]
 
 # Hashear as senhas usando o bcrypt via streamlit-authenticator
 hashed_passwords = stauth.Hasher(passwords).generate()
