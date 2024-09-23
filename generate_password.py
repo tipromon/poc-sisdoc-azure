c155b8bb1d5e9822d39fe710dd17bd253f1dfa1c
tipromon <154251431+tipromon@users.noreply.github.com>
2024-09-23 10:10:33 -0300

    Add files via upload
    Atualização de usuários

--- generate_password.py
+++ generate_password.py
@@ -3,11 +3,11 @@ import pickle
 from pathlib import Path
 
 # Nomes e usuários para autenticação
-names = ["Peter Parker", "Rebecca Miller", "Michel Daros", "Gustavo Pelissaro", "Alex Sandoval", "Alexsandra Mendes", "Marco Lamim", "Guilherme Grandesi", "Henrique Riego", "Rogerio Ishikawa", "David Andrade", "Fabiana Garcia"]
-usernames = ["pparker", "rmiller", "mdaros", "gpelissaro", "asandoval", "amendes", "mlamim", "ggrandesi", "hriego", "rishikawa", "dandrade", "fgarcia"]
+names = ["Peter Parker", "Rebecca Miller", "Michel Daros", "Gustavo Pelissaro", "Alex Sandoval", "Alexsandra Mendes", "Marco Lamim", "Guilherme Grandesi", "Henrique Riego", "Rogerio Ishikawa", "David Andrade", "Fabiana Garcia", "Gabriela Souza", "Andre Hiroshi", "Rafael Pereira"]
+usernames = ["pparker", "rmiller", "mdaros", "gpelissaro", "asandoval", "amendes", "mlamim", "ggrandesi", "hriego", "rishikawa", "dandrade", "fgarcia", "gsouza", "ahiroshi", "rpereira"]
 
 # Definir as senhas (não hashadas) que serão usadas
-passwords = ["abc123", "def456", "michelpassword", "gustavopassword", "alexpassword", "alexsandrapassword", "marcopassword", "guilherme123", "henrique123", "rogerio123", "david123", "fabiana123"]
+passwords = ["abc123", "def456", "michelpassword", "gustavopassword", "alexpassword", "alexsandrapassword", "marcopassword", "guilherme123", "henrique123", "rogerio123", "david123", "fabiana123", "gabriela123", "andre123", "rafael123"]
 
 # Hashear as senhas usando o bcrypt via streamlit-authenticator
 hashed_passwords = stauth.Hasher(passwords).generate()
