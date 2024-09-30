ed3a27b6bba63ba920743eae2f2add4ba268cd55
tipromon <154251431+tipromon@users.noreply.github.com>
2024-09-30 15:36:30 -0300

    Add files via upload
    Adição usuário Andre Hiroshi

--- generate_password.py
+++ generate_password.py
@@ -3,11 +3,11 @@ import pickle
 from pathlib import Path
 
 # Nomes e usuários para autenticação
-names = ["Peter Parker", "Rebecca Miller", "Michel Daros", "Gustavo Pelissaro", "Alex Sandoval", "Alexsandra Mendes", "Marco Lamim", "Guilherme Grandesi", "Henrique Riego", "Rogerio Ishikawa", "David Andrade", "Fabiana Garcia", "Gabriela Souza", "Andre Hiroshi", "Rafael Pereira", "Gisele Duarte", "Bruna Rufino", "Hellen Vitali", "Rosana Bretzel"]
-usernames = ["pparker", "rmiller", "mdaros", "gpelissaro", "asandoval", "amendes", "mlamim", "ggrandesi", "hriego", "rishikawa", "dandrade", "fgarcia", "gsouza", "ahiroshi", "rpereira", "gduarte", "brufino", "hvitali", "rbretzel"]
+names = ["Peter Parker", "Rebecca Miller", "Michel Daros", "Gustavo Pelissaro", "Alex Sandoval", "Alexsandra Mendes", "Marco Lamim", "Guilherme Grandesi", "Henrique Riego", "Rogerio Ishikawa", "David Andrade", "Fabiana Garcia", "Gabriela Souza", "Andre Hiroshi", "Rafael Pereira", "Gisele Duarte", "Bruna Rufino", "Hellen Vitali", "Rosana Bretzel", "Andre Hiroshi"]
+usernames = ["pparker", "rmiller", "mdaros", "gpelissaro", "asandoval", "amendes", "mlamim", "ggrandesi", "hriego", "rishikawa", "dandrade", "fgarcia", "gsouza", "ahiroshi", "rpereira", "gduarte", "brufino", "hvitali", "rbretzel", "ahiroshi"]
 
 # Definir as senhas (não hashadas) que serão usadas
-passwords = ["abc123", "def456", "michelpassword", "gustavopassword", "alexpassword", "alexsandrapassword", "marcopassword", "guilherme123", "henrique123", "rogerio123", "david123", "fabiana123", "gabriela123", "andre123", "rafael123", "giselepassword", "brunapassword", "hellenpassword", "rosanapassword"]
+passwords = ["abc123", "def456", "michelpassword", "gustavopassword", "alexpassword", "alexsandrapassword", "marcopassword", "guilherme123", "henrique123", "rogerio123", "david123", "fabiana123", "gabriela123", "andre123", "rafael123", "giselepassword", "brunapassword", "hellenpassword", "rosanapassword", "andrepassword"]
 
 # Hashear as senhas usando o bcrypt via streamlit-authenticator
 hashed_passwords = stauth.Hasher(passwords).generate()
