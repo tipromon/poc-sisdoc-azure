6b7acfb84d574c3232f4fd7b71697c7094e4f2be
tipromon <154251431+tipromon@users.noreply.github.com>
2024-10-04 10:01:53 -0300

    Update sisdocapp.py

--- sisdocapp.py
+++ sisdocapp.py
@@ -66,35 +66,9 @@ usernames = ["pparker", "rmiller", "mdaros", "gpelissaro", "asandoval", "amendes
 # Carregar as senhas hasheadas
 file_path = Path(__file__).parent / "hashed_pw.pkl"
 with file_path.open("rb") as file:
-    hashed_passwords = pickle.load(file)
+    credentials = pickle.load(file)
 
-# Configurar as credenciais de autenticação
-credentials = {
-    "usernames": {
-        usernames[0]: {"name": names[0], "password": hashed_passwords[0]},
-        usernames[1]: {"name": names[1], "password": hashed_passwords[1]},
-        usernames[2]: {"name": names[2], "password": hashed_passwords[2]},
-        usernames[3]: {"name": names[3], "password": hashed_passwords[3]},
-        usernames[4]: {"name": names[4], "password": hashed_passwords[4]},
-        usernames[5]: {"name": names[5], "password": hashed_passwords[5]},
-        usernames[6]: {"name": names[6], "password": hashed_passwords[6]},
-        usernames[7]: {"name": names[7], "password": hashed_passwords[7]},
-        usernames[8]: {"name": names[8], "password": hashed_passwords[8]},
-        usernames[9]: {"name": names[9], "password": hashed_passwords[9]},
-        usernames[10]: {"name": names[10], "password": hashed_passwords[10]},
-        usernames[11]: {"name": names[11], "password": hashed_passwords[11]},
-        usernames[12]: {"name": names[12], "password": hashed_passwords[12]},
-        usernames[13]: {"name": names[13], "password": hashed_passwords[13]},
-        usernames[14]: {"name": names[14], "password": hashed_passwords[14]},
-        usernames[15]: {"name": names[15], "password": hashed_passwords[15]},
-        usernames[16]: {"name": names[16], "password": hashed_passwords[16]},
-        usernames[17]: {"name": names[17], "password": hashed_passwords[17]},
-        usernames[18]: {"name": names[18], "password": hashed_passwords[18]},
-        usernames[19]: {"name": names[19], "password": hashed_passwords[19]}        
-    }
-}
-
-# Criar o objeto de autenticação
+# Configurar as credenciais de autenticação diretamente a partir do arquivo pickle carregado
 authenticator = stauth.Authenticate(
     credentials=credentials,
     cookie_name="promon_ai_chatbot",
